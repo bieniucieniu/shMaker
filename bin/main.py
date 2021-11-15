7143b1434a602c92f3ae711bfe1dfb1f819ba997
bieniucieniu <72389205+bieniucieniu@users.noreply.github.com>
2021-11-15 01:27:35 +0100

    first commit

---
+++ bin/main.py
@@ -0,0 +1,22 @@
+from func import *
+
+sorceFile = "sorce/solutions1.txt"
+
+
+
+if __name__ == "__main__":
+
+    with open(sorceFile, "rt", encoding="utf-8") as inputFile:
+
+        tasksAndSolutions = []
+        
+
+        for line in inputFile:
+            if(doWordsAppearInOrder(line, "Zad.")):
+                line = line.translate(str.maketrans({"\n" : "", "\t" : ""}))
+                tasksAndSolutions.append(line)
+
+            elif(doWordsAppearInOrder(line, "@", "$")):
+                line = line.translate(str.maketrans({"\n" : "", "\t" : ""}))
+                tasksAndSolutions.append(line)
+        
