c862240084715065794ab2e71b7b20f850c51f97
bieniucieniu <72389205+bieniucieniu@users.noreply.github.com>
2021-11-26 12:55:53 +0100

    sus

--- bin/main.py
+++ bin/main.py
@@ -1,15 +1,17 @@
 from func import *
 
-sorceFile = "sorce/solutions1.txt"
+sorceFile1 = "sorce/solutions1.txt"
 
+sorceFile2 = "sorce/tresc_zadan1.txt"
 
+outputFile = "output/output.txt"
 
 
 if __name__ == "__main__":
+    
+    TasksAndSolutions = []
 
-    with open(sorceFile, "rt", encoding="utf-8") as inputFile:
-        
-        TasksAndSolutions = []
+    with open(sorceFile1, "rt", encoding="utf-8") as inputFile:
         
         taskAndSolutions = ["", "", []]
 
@@ -23,5 +25,19 @@ if __name__ == "__main__":
 
             elif(doWordsAppearInStr(line, "@", "$")):
                 taskAndSolutions[2].append(line.translate(str.maketrans({"\n" : "", "\t" : ""})))
+        
+        TasksAndSolutions.append(taskAndSolutions)
+    
+    with open(sorceFile2, "rt", encoding="utf-8") as inputFile:
 
+        for indx, line in enumerate(inputFile):
+            if(doWordsAppearInStr(line, str(indx+1))):
+                line = line.translate(str.maketrans({"\n" : "", "\t" : ""}))
+                TasksAndSolutions[indx][0] = line
 
+    with open(outputFile, "wt", encoding="utf-8") as output:
+        for asd in TasksAndSolutions:
+            output.writelines(asd[0] + f"\n")
+            output.writelines(asd[1] + f"\n")
+            for lsa in asd[2]:
+                output.writelines(lsa + f"\n")
